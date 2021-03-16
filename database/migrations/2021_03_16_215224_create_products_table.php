d1666b87d2c79a2d2d895fc97970c22af305eb27
Matheus Almeida <matheusjv14@gmail.com>
2021-03-16 19:42:32 -0300

    feat: concluding migrations related to models of database

---
+++ database/migrations/2021_03_16_215224_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(column: 'category_id')->constrained();
+            $table->string(column: 'name');
+            $table->text(column: 'description');
+            $table->boolean(column: 'exclusive');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
